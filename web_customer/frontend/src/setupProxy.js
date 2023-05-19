6a6192f4eb5e9235ec3f6518b9580a2ab000d66a
Özgür Abi <ozgur.abi@ug.bilkent.edu.tr>
2023-05-19 21:38:25 +0300

    web update

--- web_customer/frontend/src/setupProxy.js
+++ web_customer/frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
       createProxyMiddleware("/api",{
-        target: 'http://104.248.26.143:8080', // The target server that you want to proxy to
+        target: 'http://localhost:8080', // The target server that you want to proxy to
         changeOrigin: true, // Needed for virtual hosted sites
           pathRewrite: {
               '^/api' : '/'
