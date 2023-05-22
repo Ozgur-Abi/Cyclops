e57eee224f51766c85f6aeaa8282bf180ac575d5
Özgür Abi <ozgur.abi@ug.bilkent.edu.tr>
2023-05-22 16:01:34 +0300

    a

--- web/src/main/java/com/app/controller/OrderController.java
+++ web/src/main/java/com/app/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.app.controller;
 
 import com.app.entity.Customer;
 import com.app.entity.FoodOrder;
+import com.app.entity.OccupiedData;
+import com.app.repository.CustomerRepository;
+import com.app.repository.OrderRepository;
 import com.app.service.CustomerService;
 import com.app.service.OrderService;
 import lombok.NonNull;
@@ -11,16 +14,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/order")
 @RequiredArgsConstructor(onConstructor = @__({@Autowired,@NonNull}))
 public class OrderController {
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/getById")
     public ResponseEntity getOrderById(@RequestParam("orderId") int orderId) {
@@ -32,5 +41,22 @@ public class OrderController {
     public ResponseEntity getAllOrders() {
         List<FoodOrder> orders = orderService.findAll();
         return new ResponseEntity(orders, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/createRandom", produces = "application/json")
+    public ResponseEntity createRandomData(){
+        int tableCount = 5;
+
+        for(int i = 0; i < 24; i++){
+            Random rand = new Random();
+            int count = rand.nextInt(5);
+            FoodOrder rOrder = new FoodOrder();
+            rOrder.setCustomer(customerRepository.getById(1));
+            rOrder.setTableId(count + 1);
+            rOrder.setOrderText("kürt böreği");
+            orderRepository.save(rOrder);
+        }
+
+        return new ResponseEntity("success", HttpStatus.OK);
     }
 }
