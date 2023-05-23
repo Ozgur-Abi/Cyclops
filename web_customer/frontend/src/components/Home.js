c7d527c6ad5c470c286144d875756016bdcb430d
Jankat Berslan Dinçer <berslan.dincer@ug.bilkent.edu.tr>
2023-05-23 13:07:20 +0300

    Updated Restaurant List

--- web_customer/frontend/src/components/Home.js
+++ web_customer/frontend/src/components/Home.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
+import {Container} from "react-bootstrap";
+import {Form} from "react-bootstrap";
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -25,12 +30,19 @@ export class Home extends Component {
       <div>
         <h3 style={{textAlign: 'center'}} className="mt-3">Welcome to Cyclops!</h3>
         <p style={{textAlign: 'center'}}>Please choose the restaurant you would like to see details of:</p>
-        <ul>
+          <ul>
             {restaurantList.map(restaurant => (
-                <li> <a href = {"/restaurant/" + restaurant.restaurantNo}>{restaurant.restaurantNo + ": " + restaurant.restaurantName}</a></li>
+                <ListGroup horizontal className="d-flex justify-content-center">
+                    <ListGroup.Item>
+                        <Card style={{ width: '28rem'}} >
+                            <a class="btn btn-primary" href = {"/restaurant/" + restaurant.restaurantNo}>{restaurant.restaurantNo + ": " + restaurant.restaurantName}</a>
+                        </Card>
+                    </ListGroup.Item>
+                </ListGroup>
             ))}
         </ul>
       </div>
     );
   }
 }
+//<li> <a href = {"/restaurant/" + restaurant.restaurantNo}>{restaurant.restaurantNo + ": " + restaurant.restaurantName}</a></li>
